f204308b7820bfa6a423887149dec0eee366b3b0
nsdq12345 <yeungjian@gmail.com>
2019-07-23 12:30:20 -0700

    added basic hovering functionality

--- src/bigDancer.js
+++ src/bigDancer.js
@@ -15,12 +15,20 @@ makeBigDancer.prototype.step = function(){
     //console.log(typeof oldStep);
     //var oldStep = this.step;
     oldStep();
-    this.$node.toggle(); 
-    this.$node.hover(function() {
-        console.log(this);
-        this.$node.css('background-color', 'white');
-    });
-    //})
+
+    var hoverReact = function() {
+        if (this.$node.hasClass("white-dancer") || this.$node.hasClass("big-dancer")) {
+            this.$node.addClass("red-dancer");
+            this.$node.removeClass("big-dancer");
+            this.$node.removeClass("white-dancer");
+        } else if (this.$node.hasClass("red-dancer")) {
+            this.$node.addClass("white-dancer");
+            this.$node.removeClass("big-dancer");
+            this.$node.removeClass("red-dancer");
+        }
+    }
+    
+    this.$node.hover(hoverReact.bind(this));
 }
 
 
